6b79119ab03697333f56fb1f68bf782fcf3f1e88
rileyg98 <rileyg98@users.noreply.github.com>
2020-01-27 21:35:24 +1000

    Fix open on install

--- src/background/runtime.background.ts
+++ src/background/runtime.background.ts
@@ -380,7 +380,7 @@ export default class RuntimeBackground {
         setTimeout(async () => {
             if (this.onInstalledReason != null) {
                 if (this.onInstalledReason === 'install') {
-                    BrowserApi.createNewTab('https://bitwarden.com/browser-start/');
+                    BrowserApi.createNewTab('https://vault.vivokey.com/get-started');
                     await this.setDefaultSettings();
                 }
 
